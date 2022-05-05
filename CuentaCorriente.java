96f8b52103e1014867821a1b66d7f71e032f4fcd
Salvador Martos <smarbau@gmail.com>
2022-05-05 20:18:14 +0200

    Primer commit

---
+++ CuentaCorriente.java
@@ -0,0 +1,37 @@
+package Ejercicio_1;
+
+public class CuentaCorriente {
+	String dni;
+	String nombre;
+	double saldo;		
+
+	CuentaCorriente (String dni, String nombre) {
+		this.dni = dni;
+		this.nombre = nombre;
+		double saldo = 0;
+	}
+	
+	boolean reintegro (double cant) {
+		boolean operacionPosible;
+		if (saldo >= cant) {
+			saldo -= cant;
+			operacionPosible = true;
+		} else {
+			System.out.println("No hay dinero suficiente.");
+			operacionPosible = false;
+		}
+		return operacionPosible;
+	}
+	
+	void ingreso (double cant) {
+		saldo += cant;
+	}
+	
+	void mostrarInformacion () {
+		
+		System.out.println("Nombre: " + nombre);
+		System.out.println("DNI: " + dni);
+		System.out.println("Saldo: " + saldo);
+		System.out.println("..................................");
+	}
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+package Ejercicio_1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CuentaCorriente c;
+		c = new CuentaCorriente("12345568-A", "Pepe");
+		c.ingreso(1000);
+		c.reintegro(100);
+		c.mostrarInformacion();
+		System.out.println("Puedo sacar 700€: " + c.reintegro(700));
+		System.out.println("Puedo sacar 500€: " + c.reintegro(500));
+	}
+	
+}
